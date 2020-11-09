5d317e1ebaaf42ccd51d1863024236466dd20884
susana <susana.casas@iesdoctorbalmis.com>
2020-11-09 10:06:52 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio7_Formulario/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Ejercicio7_Formulario
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
